019afae61866712d68237aa8d2b7dc734ff5193e
Anie <popova_ana@yahoo.com>
2019-06-23 12:17:42 +0300

    Code updates

--- R/01.MAIN.R
+++ R/01.MAIN.R
@@ -4,12 +4,24 @@ options(scipen = 999)
 
 #-----------------------------------------------------------------------------------------------
 # Load libraries
+library(sjmisc)
 
 library(tidyverse)
 library(lubridate)
 library(anytime)
+library(skimr)  
 
 library(readxl)
 library(openxlsx)
 
 library(reshape2)
+
+library(anomalize)
+
+library(recommenderlab)
+
+
+# library(data.tree)
+# library(treemap)
+# library(networkD3)
+# library(igraph)
--- R/03.DataPrep.R
+++ R/03.DataPrep.R
@@ -75,29 +75,154 @@ ComboSet <- Events %>%
             (Linked %>% select(itemid, categoryid,parentid)),
             by = "itemid")
 
-write_csv(ComboSet, "Output/ComboSet.csv")
+
+
+# to dummy from events
+tmp <- ComboSet %>% 
+  select(event) %>% 
+  to_dummy(., suffix = "label")
+
+ComboSet <- ComboSet %>% 
+  bind_cols(., tmp)
 
 info <- infoNA(ComboSet)
+write_csv(ComboSet, "Output/ComboSet.csv")
+
+ComboSet %>% skim()
+
+ComboSet %>% 
+  filter(event_view <= 0) %>% 
+  group_by(visitorid, week) %>% 
+  summarise(count =n()) %>%
+  arrange(desc(count)) %>% 
+  ungroup()
+
+ComboSet %>% 
+  filter(event_addtocart <= 0) %>% 
+  group_by(visitorid, week) %>% 
+  summarise(count =n()) %>%
+  arrange(desc(count)) %>% 
+  ungroup()
+
+ComboSet %>% 
+  group_by(visitorid) %>% 
+  summarize(count = n()) %>% 
+  top_n(10, wt = count) %>%
+  arrange(desc(count)) %>% 
+  ggplot(aes(x = reorder(visitorid, count), y = count))+
+  geom_bar(stat = "identity", fill = "royalblue", colour = "blue") +
+  labs(x = "", y = "Top 10 Best Sellers", title = "Most Ordered Products") +
+  coord_flip() +
+  theme_grey(base_size = 12)
+
+ComboSet <- ComboSet %>% 
+  mutate(tod = hour(timeItemCats))
+
+
+
+# ------------------------------------------------------------------------------
+
+# Establish event history
+
+# Create a user-item matrix
+Test <- ComboSet %>% 
+  group_by(visitorid, week)
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------
+# pick pov - view, add to cart, purchase
+
+# - visitor
+# - items
+
 
 # Check how many unique item-cat-parent combos there are
-test <- ComboSet %>% 
-  group_by(itemid, categoryid, parentid) %>% 
-  summarise(count = n())
-test2 <- test %>% 
-  filter(is.na(categoryid) & is.na(parentid))  # items with no cat and parent
-
-
-
-# ------------------------------------------------------------------------------  
-# TestProp <- Items %>% 
-#   filter(property %in% c("categoryid", "available")) %>% 
-#   group_by(timestamp, itemid) %>% 
-#   filter(property == "categoryid") %>% 
+# test <- ComboSet %>% 
+#   group_by(itemid, categoryid, parentid) %>% 
 #   summarise(count = n())
+# test2 <- test %>% 
+#   filter(is.na(categoryid) & is.na(parentid))  # 49815 items with no cat and parent
 # 
-# TestAv <- Items %>% 
-#   filter(property %in% c("categoryid", "available")) %>% 
-#   group_by(timestamp, itemid) %>% 
-#   filter(property == "available") %>% 
-#   summarise(count = n())
+# # Look into visitors
+# length(unique(ComboSet$visitorid))  # 1407580
 
+
+#event, category, time, availability
+
+
+
+
+
+
+# # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# # Detailed look at category tree
+# 
+# test <- CatTree %>% 
+#   left_join(., ., by = c("parentid", "categoryid")) %>% 
+#   left_join(., ., by = "parentid") 
+#   
+# 
+# 
+# 
+# parent <- CatTree %>% 
+#   pull(parentid)
+# 
+# child <- CatTree %>% 
+#   pull(categoryid)
+# 
+# z <- setdiff(aa, a)
+# intersecting <- intersect(parent, child) %>% 
+#   enframe(name = NULL, value = "Intersect") %>% 
+#   left_join(., CatTree$parentid, by = "")
+# 
+# 
+# 
+# 
+# net <- graph_from_data_frame(d = CatTree,
+#                              directed = T)
+# plot(net)
+# 
+# test <- CatTree %>% 
+#   select(parentid, categoryid) %>% 
+#   as.data.frame() %>% 
+#   spread(parentid, categoryid)
+# 
+# library(data.tree)
+# tree <- FromDataFrameNetwork(test)
+# tree
+# lol <- ToListExplicit(tree, unname = TRUE)
+# diagonalNetwork(lol)
+# 
+# test <- CatTree
+# 
+# 
+# test <- CatTree #%>% 
+#   # group_by(categoryid, parentid) %>% 
+#   # summarise(Count = n())
+# 
+# 
+# parent <- unique(CatTree$parentid) %>% 
+#   enframe(., name = NULL, value = "parentid") %>% 
+#   left_join(., CatTree, by = "parentid")
+# 
+# 
+# 
+# 
+# # ------------------------------------------------------------------------------  
+# # TestProp <- Items %>% 
+# #   filter(property %in% c("categoryid", "available")) %>% 
+# #   group_by(timestamp, itemid) %>% 
+# #   filter(property == "categoryid") %>% 
+# #   summarise(count = n())
+# # 
+# # TestAv <- Items %>% 
+# #   filter(property %in% c("categoryid", "available")) %>% 
+# #   group_by(timestamp, itemid) %>% 
+# #   filter(property == "available") %>% 
+# #   summarise(count = n())
+# 
